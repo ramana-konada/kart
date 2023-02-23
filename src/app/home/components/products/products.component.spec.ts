fc63015bd059af4bc3f09c67870deab4a725ec26
ramana-konada <konada.ramana25@gmail.com>
2023-02-24 01:58:40 +0530

    edited(component-headercomponent): added translation

--- src/app/home/components/products/products.component.spec.ts
+++ src/app/home/components/products/products.component.spec.ts
@@ -1,23 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from "@angular/common";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Observable, of } from "rxjs";
+import { ProductsService } from "../../services/products/products.service";
 
-import { ProductsComponent } from './products.component';
+import { ProductsComponent } from "./products.component";
 
-describe('ProductsComponent', () => {
+class ProductsServiceMock {
+  getProducts(): Observable<any> {
+    return of(null);
+  }
+}
+describe("ProductsComponent", () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let service: ProductsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductsComponent],
+      imports: [CommonModule],
+      providers: [{ provide: ProductsService, useClass: ProductsServiceMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
+    service = TestBed.inject(ProductsService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should check ngOninit", () => {
+    service.getProducts().subscribe((s: any) => {
+      expect(s).toBeNull();
+      expect(component.productsList).toBeNull();
+    });
   });
 });
