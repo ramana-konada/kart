0ac0993b54e418b8ced50d7f9416d603b5ce2bab
ramana-konada <konada.ramana25@gmail.com>
2023-02-28 01:35:22 +0530

    adjusted(module-homemodule): modifications done

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,12 +7,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { MenuHeaderComponent } from "./components/menu-header/menu-header.component";
 import { ProductsComponent } from "./components/products/products.component";
 import { LoginComponent } from "./components/login/login/login.component";
-import { FormatPipe } from "./pipes/format.pipe";
 import { MobilesStoreComponent } from "./components/mobiles-store/mobiles-store.component";
 import { MobileComponent } from "./components/mobile/mobile.component";
 import { FilterComponent } from "./components/mobile/filter/filter.component";
 import { DashboardComponent } from "./components/mobile/dashboard/dashboard.component";
 import { MobileDetailComponent } from "./components/mobile-detail/mobile-detail.component";
+import { FormatPipe } from "./pipes/format.pipe";
 
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatInputModule } from "@angular/material/input";
