1e008ac9ae0a8a3d1eeb1998c7fc7c084ee2c1e9
ramana-konada <konada.ramana25@gmail.com>
2023-03-03 13:35:31 +0530

    (module-appmodule): appmoduleupdated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HomeModule } from "./home/home.module";
-import { KheaderService } from "./home/services/kheader/kheader.service";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+
 import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { EffectsModule } from "@ngrx/effects";
 import { HomeEffect } from "./home/store/effects/home.effect";
-import { environment } from "src/environments/environment";
 import { MobileEffect } from "./home/store/effects/mobile.effect";
 import { kartReducer } from "./home/store/reducers/kart-bundle.reducer";
-import { AdminModule } from "./admin/admin.module";
-import { adminReducer } from "./admin/store/admin.reducer";
 import { orderListReducer } from "./admin/store/orderlist-store/orderlist.reducer";
 import { OrderListEffect } from "./admin/store/orderlist-store/orderlist.effect";
+import { adminReducer } from "./admin/store/admin.reducer";
+import { HomeModule } from "./home/home.module";
+import { AdminModule } from "./admin/admin.module";
+import { KheaderService } from "./home/services/kheader/kheader.service";
+import { environment } from "src/environments/environment";
 
 const NGRXDevTools = environment.production
   ? []
